20d916fe1c0b3a5df3572326a9b4d074419d3e3f
Eukaa <39858903+Eukaa@users.noreply.github.com>
2025-02-23 20:23:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ if ingredients_list:
         ingredients_string+=fruit_choosen + ' '
         
     #st.write(ingredients_string)
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
